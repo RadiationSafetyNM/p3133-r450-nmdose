7de545ac741c5c108239bef030931bd7d0d8d1e0
benkorea <benkorea.ai@gmail.com>
2025-06-15 00:36:56 +0900

    first release

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8")
 
 # requirements.txt에서 의존성 읽기
 install_requires = [
-    req.strip() for req in (HERE / "requirements.txt").read_text().splitlines()
-    if req.strip() and not req.startswith("#")
+    "iniconfig==2.1.0",
+    "packaging==25.0",
+    "pluggy==1.6.0",
+    "Pygments==2.19.1",
+    "pytest==8.4.0",
+    "PyYAML==6.0.2"
 ]
 
 setup(
